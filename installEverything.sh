5079ff6abd72a93f8495c3d752f830085da18b30
hourglass <56056461+hourglass492@users.noreply.github.com>
2022-09-02 12:50:23 -0400

    Update installEverything.sh

--- installEverything.sh
+++ installEverything.sh
@@ -6,3 +6,13 @@ yes | sudo apt install $l || echo "Could not install $l"
 done < Install_list.txt
 
 popd
+
+mkdir ~/.bin
+
+pushd ~./bin
+
+git clone https://github.com/mubaris/motivate.git
+cd motivate/motivate
+sudo ./install.sh
+source ~/.bashrc
+popd
