94ee48f16f8ce5d5139c4fde28461d83b1c7f16f
hourglass492 <hourglass@fake.com>
2022-09-05 12:39:31 -0400

    added install motivate

--- install.sh
+++ install.sh
@@ -3,3 +3,12 @@ for i in $(cat Install_list.txt); do
 	echo "installing $i"
 	yes | sudo apt-get install $i
 done
+
+
+# install motivate
+mkdir -p ~/.bin
+pushd ~/.bin
+git clone https://github.com/mubaris/motivate.git
+cd motivate/motivate
+sudo ./install.sh
+popd
